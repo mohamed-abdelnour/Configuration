a8b9d00402b0c4538fef534f7f56ec1c30dcf22d
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-04-29 15:07:22 +0200

    Add keybindings

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -13,6 +13,15 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
+    if client.resolved_capabilities.document_formatting then
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>",
+                       opts)
+    end
+    if client.resolved_capabilities.document_range_formatting then
+        buf_set_keymap("v", "<space>f",
+                       "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+    end
+
     require('completion').on_attach()
 end
 
