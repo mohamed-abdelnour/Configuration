d16e46152aef9024fc2dcd61c44c8df306940d41
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-05-27 15:11:17 +0200

    Fix formatting

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -2,8 +2,9 @@ local nvim_lsp = require('lspconfig')
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport =
-    {properties = {'documentation', 'detail', 'additionalTextEdits'}}
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {'documentation', 'detail', 'additionalTextEdits'}
+}
 
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...)
