399e1d7e6aa9630b702cf5707f0c8a34aad2127e
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-06-10 00:14:58 +0200

    Migrate to `telescope.nvim`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,14 +3,15 @@ return require('packer').startup(function()
     use 'dstein64/vim-startuptime'
     use 'hrsh7th/nvim-compe'
     use 'joshdick/onedark.vim'
-    use 'junegunn/fzf'
-    use 'junegunn/fzf.vim'
     use 'lervag/vimtex'
-    use 'mcchrish/nnn.vim'
     use 'neovim/nvim-lspconfig'
     use 'neovimhaskell/haskell-vim'
     use 'sbdchd/neoformat'
     use 'wbthomason/packer.nvim'
     use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install'}
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    }
 end)
--- .config/nvim/plugin/nnn.vim
+++
@@ -1,8 +0,0 @@
-let g:nnn#set_default_mappings = 0
-nnoremap <silent> <C-n> :NnnPicker<CR>
-
-let g:nnn#action = {
-            \ '<c-t>': 'tab split',
-            \ '<c-x>': 'split',
-            \ '<c-v>': 'vsplit'
-            \ }
