dec5a740676077dfa7603458c54cd8d6233744bf
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-07-31 06:53:41 +0200

    Add functions

--- .config/emacs/modules/init-functions.el
+++ .config/emacs/modules/init-functions.el
@@ -10,6 +10,21 @@
   "Return string with SUFFIX appended to Emacs's configuration directory."
   (format "%s%s" (expand-file-name "~/.config/emacs") suffix))
 
+(defun keyboard-yes-or-no-p (prompt)
+  "Set `last-nomenu-event' to 0 and pass PROMPT to `yes-or-no-p'.
+This forces the use of keyboard input for `yes-or-no-p'."
+  (setq last-nonmenu-event 0)
+  (yes-or-no-p prompt))
+
+(defun plain-font (face)
+  "Make FACE plain."
+  (set-face-attribute face nil :weight 'normal :slant 'normal))
+
+(defun plain-fonts ()
+  "Make all fonts plain."
+  (interactive)
+  (mapc 'plain-font (face-list)))
+
 (provide 'init-functions)
 
 ;;; init-functions.el ends here
