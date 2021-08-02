4b81d8740426eb568ef6e84f8b4daaf1a10a8fd4
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-08-02 08:32:02 +0200

    Update `lsp-mode` configuration

--- .config/emacs/modules/init-lsp.el
+++ .config/emacs/modules/init-lsp.el
@@ -6,24 +6,35 @@
 
 ;;; Code:
 
+(defun configure-lsp ()
+  (lsp-completion--enable)
+  (lsp-diagnostics--enable)
+  (lsp-modeline-code-actions-mode)
+  (lsp-modeline-diagnostics-mode)
+  (lsp-ui-mode))
+
 (use-package lsp-mode
   :commands lsp
   :config (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
   :custom
+  (lsp-auto-configure nil)
   (lsp-completion-provider :none)
   (lsp-enable-folding nil)
   (lsp-enable-text-document-color nil)
   (lsp-headerline-breadcrumb-enable nil)
-  (lsp-idle-delay 1.0)
   (lsp-keep-workspace-alive nil)
   (lsp-signature-auto-activate nil)
-  :hook ((lsp-mode . lsp-enable-which-key-integration)))
+  :hook ((lsp-configure . configure-lsp)
+         (lsp-mode . lsp-enable-which-key-integration)))
 
 (use-package lsp-ui
   :bind ("C-c k" . lsp-ui-doc-glance)
   :custom
   (lsp-ui-doc-enable nil)
-  (lsp-ui-sideline-delay 1.0))
+  (lsp-ui-doc-position 'at-point)
+  (lsp-ui-imenu-enable nil)
+  (lsp-ui-peek-enable nil)
+  (lsp-ui-sideline-update-mode 'line))
 
 (provide 'init-lsp)
 
