ab2f7ff5e83b9a681c108615f6bfa55b259a7c4b
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-08-06 05:27:19 +0200

    Auto-configure `lsp-mode`

--- .config/emacs/modules/init-lsp.el
+++ .config/emacs/modules/init-lsp.el
@@ -6,27 +6,19 @@
 
 ;;; Code:
 
-(defun configure-lsp ()
-  (lsp-completion--enable)
-  (lsp-diagnostics--enable)
-  (lsp-modeline-code-actions-mode)
-  (lsp-modeline-diagnostics-mode))
-
 (use-package consult-lsp)
 
 (use-package lsp-mode
   :commands lsp
   :config (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
   :custom
-  (lsp-auto-configure nil)
   (lsp-completion-provider :none)
   (lsp-enable-folding nil)
   (lsp-enable-text-document-color nil)
   (lsp-headerline-breadcrumb-enable nil)
   (lsp-keep-workspace-alive nil)
   (lsp-signature-auto-activate nil)
-  :hook ((lsp-configure . configure-lsp)
-         (lsp-mode . lsp-enable-which-key-integration)))
+  :hook (lsp-mode . lsp-enable-which-key-integration))
 
 (provide 'init-lsp)
 
