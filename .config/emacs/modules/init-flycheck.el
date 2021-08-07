ccb0db90d96618d1d160d3136d9b814b0f4db267
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-08-07 10:21:11 +0200

    Flycheck updates

--- .config/emacs/modules/init-flycheck.el
+++ .config/emacs/modules/init-flycheck.el
@@ -10,7 +10,12 @@
 (use-package consult-flycheck)
 
 (use-package flycheck
-  :config (add-to-list 'flycheck-shellcheck-supported-shells 'dash)
+  :config
+  (add-to-list 'flycheck-shellcheck-supported-shells 'dash)
+  (delq 'new-line flycheck-check-syntax-automatically)
+  :custom
+  (flycheck-display-errors-delay 0.25)
+  (flycheck-idle-change-delay 1.0)
   :init (setq-default flycheck-disabled-checkers '(c/c++-clang
                                                    c/c++-cppcheck
                                                    c/c++-gcc)))
