890abfaaa847d8f966bf38dc2e3228700461b801
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-11-30 12:05:05 +0200

    feat: configure incremental selection

--- .config/nvim/lua/plugins/init_treesitter.lua
+++ .config/nvim/lua/plugins/init_treesitter.lua
@@ -1,4 +1,13 @@
 require("nvim-treesitter.configs").setup({
     highlight = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_decremental = "grm",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+        },
+    },
     rainbow = { enable = true },
 })
