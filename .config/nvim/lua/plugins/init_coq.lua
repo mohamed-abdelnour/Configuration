b97427dcd6103eb6ae594c4ac8ff0b532f79f89a
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-12-01 12:14:24 +0200

    style: split table onto multiple lines

--- .config/nvim/lua/plugins/init_coq.lua
+++ .config/nvim/lua/plugins/init_coq.lua
@@ -1,4 +1,7 @@
 vim.g.coq_settings = {
     auto_start = "shut-up",
-    display = { icons = { mode = "none" }, pum = { fast_close = false } },
+    display = {
+        icons = { mode = "none" },
+        pum = { fast_close = false },
+    },
 }
