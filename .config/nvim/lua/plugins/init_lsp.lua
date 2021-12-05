2694a5e23ae1d6371977b570da66d97ad65f8869
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-12-05 07:21:50 +0200

    feat: allow configuring server settings

--- .config/nvim/lua/plugins/init_lsp.lua
+++ .config/nvim/lua/plugins/init_lsp.lua
@@ -1,7 +1,20 @@
 local lsp = require("lspconfig")
 local lsp_setup = require("plugins/init_lsp_setup")
 
-local servers = { "hls", "pyright" }
-for _, server in ipairs(servers) do
+local function define_server(server, settings)
+    return { server = server, settings = settings or {} }
+end
+
+local function init_server(server, settings)
+    lsp_setup.settings = settings or {}
     lsp[server].setup(lsp_setup)
+end
+
+local servers = {
+    define_server("hls"),
+    define_server("pyright"),
+}
+
+for _, server in ipairs(servers) do
+    init_server(server.server, server.settings)
 end
