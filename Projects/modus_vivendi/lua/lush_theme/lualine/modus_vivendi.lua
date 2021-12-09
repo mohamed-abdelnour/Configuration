1d77d56008d3913d352f68448988bc8dd59a49e8
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-12-09 10:21:07 +0200

    feat: add lualine theme

---
+++ Projects/modus_vivendi/lua/lush_theme/lualine/modus_vivendi.lua
@@ -0,0 +1,28 @@
+local p = require("lush_theme/palette")
+
+local function make_section(bg, fg)
+    return { bg = bg, fg = fg }
+end
+
+local function make_mode(bg, fg)
+    local section = make_section(bg, fg)
+    local modes = { "a", "b", "c", "x", "y", "z" }
+    local result = {}
+    for _, mode in ipairs(modes) do
+        result[mode] = section
+    end
+    return result
+end
+
+local function make_modes()
+    local active_section = make_mode(p.bg_active, p.fg_active)
+    local active_modes = { "normal", "insert", "visual", "replace", "command" }
+    local result = {}
+    for _, active_mode in ipairs(active_modes) do
+        result[active_mode] = active_section
+    end
+    result.inactive = make_mode(p.bg_inactive, p.fg_inactive)
+    return result
+end
+
+return make_modes()
