7f9a52176d060c9e953ecdb383a7f79fb18892da
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-12-10 15:01:08 +0200

    feat: run clippy on save

--- .config/nvim/lua/plugins/init_rust_tools.lua
+++ .config/nvim/lua/plugins/init_rust_tools.lua
@@ -1,5 +1,13 @@
 local lsp_setup = require("plugins/init_lsp_setup").lsp_setup()
 
+lsp_setup.settings = {
+    ["rust-analyzer"] = {
+        checkOnSave = {
+            command = "clippy",
+        },
+    },
+}
+
 require("rust-tools").setup({
     tools = {
         inlay_hints = {
