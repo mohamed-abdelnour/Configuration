d1cee29a398632e8e9a4bad05b2937dc32a50495
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-12-24 11:57:33 +0200

    feat: set spell for gitcommit files

--- .config/nvim/lua/init_autocommands.lua
+++ .config/nvim/lua/init_autocommands.lua
@@ -1,6 +1,7 @@
 vim.cmd([[
     augroup term
         autocmd!
+        autocmd FileType gitcommit setlocal spell
         autocmd TermOpen * setlocal nornu
         autocmd TermOpen * setlocal signcolumn=no
         autocmd TermOpen * startinsert
