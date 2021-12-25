e5bd8f9ae7eaa60de7980ae9a44cd5832d79f1c7
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2021-12-25 16:55:41 +0200

    feat: use lua-language-server

--- .config/nvim/lua/plugins/init_lsp.lua
+++ .config/nvim/lua/plugins/init_lsp.lua
@@ -84,9 +84,45 @@ local function init_server(arg)
     lsp[arg.name].setup(lsp_setup)
 end
 
+local extend = require("init_functions").extend
+
+local sumneko_lua = {
+    name = "sumneko_lua",
+    cmd = {
+        "lua-language-server",
+        table.concat({
+            "--logpath=",
+            vim.fn.stdpath("cache"),
+            "/sumneko",
+        }),
+    },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+                path = extend(vim.split(package.path, ";"), {
+                    "lua/?.lua",
+                    "lua/?/init.lua",
+                }),
+            },
+            diagnostics = {
+                disable = { "lowercase-global" },
+                globals = { "vim" },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
+
 local servers = {
     { name = "hls" },
     { name = "pyright" },
+    sumneko_lua,
 }
 
 for _, server in ipairs(servers) do
