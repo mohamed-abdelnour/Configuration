6ae6e616773cd412c4b6c690670157ed2ed958a6
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-01-04 12:25:22 +0200

    feat: set file type for EJS files to HTML

--- .config/nvim/lua/plugins/init_filetype.lua
+++ .config/nvim/lua/plugins/init_filetype.lua
@@ -3,6 +3,7 @@ vim.g.did_load_filetypes = 1
 require("filetype").setup({
     overrides = {
         extensions = {
+            ejs = "html",
             xinitrc = "sh",
         },
         shebang = {
