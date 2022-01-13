b28f03e685902be5a6ea28a9614a60186826b076
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-01-13 08:19:36 +0200

    chore: update Emacs and Neovim

--- .config/nixpkgs/overlays/emacs.nix
+++ .config/nixpkgs/overlays/emacs.nix
@@ -1,5 +1,5 @@
 import (
   builtins.fetchTarball {
-    url = https://github.com/nix-community/emacs-overlay/archive/29631d89adfa8b3cdf35d2f65ce34a0b9dffdb20.tar.gz;
+    url = https://github.com/nix-community/emacs-overlay/archive/ba90d4ee66469dced4f2de1749d41e3551558934.tar.gz;
   }
 )
--- .config/nixpkgs/overlays/neovim.nix
+++ .config/nixpkgs/overlays/neovim.nix
@@ -3,7 +3,7 @@ _: _: {
     overlays = [
       (import
         (builtins.fetchTarball {
-          url = https://github.com/nix-community/neovim-nightly-overlay/archive/945e2862cacd95c06ff444c44e83f1539653fa01.tar.gz;
+          url = https://github.com/nix-community/neovim-nightly-overlay/archive/b8308a5693b3b027d75a16ea65dca8d98619027b.tar.gz;
         }))
     ];
   }).neovim-nightly;
