db575fb8768bb782699a55db9295b731dc68ad7d
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-02-02 10:52:26 +0200

    feat: experiment with pinning Emacs version

--- .config/nixpkgs/overlays/emacs.nix
+++ .config/nixpkgs/overlays/emacs.nix
@@ -1,5 +1,15 @@
-import (
-  builtins.fetchTarball {
-    url = https://github.com/nix-community/emacs-overlay/archive/ba90d4ee66469dced4f2de1749d41e3551558934.tar.gz;
-  }
-)
+_: _: {
+  emacsPgtkGcc = (import
+    (builtins.fetchTarball {
+      name = "nixpkgs-unstable-2022-02-01";
+      url = "https://github.com/nixos/nixpkgs/archive/5efc8ca954272c4376ac929f4c5ffefcc20551d5.tar.gz";
+    })
+    {
+      overlays = [
+        (import
+          (builtins.fetchTarball {
+            url = "https://github.com/nix-community/emacs-overlay/archive/7ba0cd76c4e5cbd4a8ac7387c8ad2493caa800f0.tar.gz";
+          }))
+      ];
+    }).emacsPgtkGcc;
+}
