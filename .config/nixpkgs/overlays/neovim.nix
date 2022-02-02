44786e92b3a0d78bc9e43c2905f3e6004de180d4
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-02-02 10:51:42 +0200

    refactor: update Neovim

--- .config/nixpkgs/overlays/neovim.nix
+++ .config/nixpkgs/overlays/neovim.nix
@@ -3,7 +3,7 @@ _: _: {
     overlays = [
       (import
         (builtins.fetchTarball {
-          url = https://github.com/nix-community/neovim-nightly-overlay/archive/b8308a5693b3b027d75a16ea65dca8d98619027b.tar.gz;
+          url = "https://github.com/nix-community/neovim-nightly-overlay/archive/19311b9ef8f8f8c71b2f41b59ec9e436646b667c.tar.gz";
         }))
     ];
   }).neovim-nightly;
