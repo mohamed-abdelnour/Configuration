db66dced6e302a087b1b437cc3d7bfbb832a6d5e
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-02-11 05:37:15 +0200

    refactor: do not require in autocmd

--- .config/nvim/lua/modules/init_autocommands.lua
+++ .config/nvim/lua/modules/init_autocommands.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 local main = function()
+    require("modules/init_term")
     vim.cmd([[
         augroup INIT
             autocmd!
-            autocmd TermOpen * lua require("modules/init_term").term_open()
+            autocmd TermOpen * lua package.loaded["modules/init_term"].term_open()
         augroup END
     ]])
 end
