bb53e89c7eb4f6444fdf5521b39a8022328a2af4
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-05-09 20:47:12 +0200

    use eclipse.jdt.ls and nvim-jdtls

--- .config/nvim/lua/plugins/languages.lua
+++ .config/nvim/lua/plugins/languages.lua
@@ -4,6 +4,11 @@ local M = {
         ft = "haskell",
     },
 
+    java = {
+        "mfussenegger/nvim-jdtls",
+        ft = "java",
+    },
+
     markdown = {
         "iamcco/markdown-preview.nvim",
         ft = "markdown",
@@ -27,6 +32,22 @@ local M = {
         },
     },
 }
+
+M.java.config = function()
+    local root = require("jdtls/setup").find_root({ "gradlew", ".git" })
+
+    local workspace = table.concat({
+        os.getenv("XDG_DATA_HOME"),
+        "/java/jdtls/workspace/",
+        vim.fn.fnamemodify(root, ":p:h:t"),
+    })
+
+    local setup = require("modules/lsp").lsp_setup({
+        cmd = { "jdtls", "-data", workspace },
+    })
+
+    require("jdtls").start_or_attach(setup)
+end
 
 M.markdown.config = function()
     vim.keymap.set("n", "<leader>m", "<cmd>MarkdownPreviewToggle<cr>", { silent = true })
