fee23c572fcfb7904463ed50ab7b24e732b69bbf
Mohamed Abdelnour <mohamed.k.abdelnour@gmail.com>
2022-07-17 11:57:43 +0200

    refactor: update `heirline` config

--- .config/nvim/lua/plugins/heirline.lua
+++ .config/nvim/lua/plugins/heirline.lua
@@ -6,165 +6,156 @@ local M = {
 
         config = function()
             Error:guard(function()
+                local ICON = require("modules.icons").U_25CF .. " "
+
                 local conditions = require("heirline.conditions")
-                local utils = require("heirline.utils")
 
                 local palette = require("modus_themes").palette
-                local icon = require("modules.icons").U_25CF .. " "
 
                 local align = { provider = "%=" }
                 local space = { provider = " " }
+                local truncate = { provider = "%<" }
 
-                local normalise_count = function(arg)
-                    arg.c = arg.c or 0
-                    if arg.c ~= 0 then
-                        return table.concat({ arg.l or "", arg.s, arg.c, arg.r or "" })
-                    end
-                    return ""
-                end
-
-                local intersperse_space = function(t)
-                    return Table.from(t):iter():intersperse(space):collect()
-                end
-
-                local generate_section = function(arg)
-                    local result = Table.default()
-                    Table.from(arg.map):iter():for_each(function(v)
-                        result:push({
-                            hl = { fg = v.fg },
-                            provider = function(self)
-                                return arg.f(self, v)
-                            end,
-                        })
-                    end)
-                    return result
-                end
-
-                local control_map = { ["\19"] = "^S", ["\22"] = "^V" }
-                local mode = {
-                    provider = function()
-                        return vim.fn.mode(1):gsub("%c", control_map):wrap()
-                    end,
-                }
-
-                local file_modifiers = function(arg)
+                local mode = function()
+                    local control_map = { ["\19"] = "^S", ["\22"] = "^V" }
                     return {
-                        condition = arg.predicate,
-                        hl = { fg = arg.fg },
-                        provider = icon,
+                        provider = function()
+                            return vim.fn.mode(1):gsub("%c", control_map):wrap()
+                        end,
                     }
                 end
-                local file = {
-                    file_modifiers({
-                        fg = palette.blue_alt_other,
-                        predicate = function()
-                            return vim.bo.modified
+
+                local file = function()
+                    local SUFFIX_LENGTH = 40
+
+                    local modifier = function(predicate, foreground)
+                        return {
+                            condition = predicate,
+                            hl = { fg = foreground },
+                            provider = ICON,
+                        }
+                    end
+
+                    local read_only = modifier(function()
+                        return not vim.bo.modifiable or vim.bo.readonly
+                    end, palette.red_alt_other)
+
+                    local buffer_name = {
+                        hl = function()
+                            return vim.bo.modified and { fg = palette.cyan }
                         end,
-                    }),
-                    file_modifiers({
-                        fg = palette.red_alt_other,
-                        predicate = function()
-                            return not vim.bo.modifiable or vim.bo.readonly
-                        end,
-                    }),
-                    {
                         provider = function()
                             local buffer = vim.api.nvim_buf_get_name(0)
-                            local file_name = vim.fn.fnamemodify(buffer, ":~:.")
-                            if not conditions.width_percent_below(#file_name, 0.25) then
-                                return vim.fn.pathshorten(file_name)
-                            end
-                            return file_name
+                            return vim.fn.fnamemodify(buffer, ":~:."):sub(-SUFFIX_LENGTH)
                         end,
-                    },
-                    { provider = "%<" },
-                }
-
-                local git_signs = function()
-                    local arg = {}
-                    arg.map = {
-                        { type = "added", sign = "+", fg = palette.green_alt_other },
-                        { type = "changed", sign = "~", fg = palette.yellow_alt_other },
-                        { type = "removed", sign = "-", fg = palette.red_alt_other },
                     }
-                    arg.f = function(self, v)
-                        return normalise_count({ c = self.status[v.type], s = v.sign, r = " " })
-                    end
-                    return generate_section(arg)
-                end
-                local git = {
-                    condition = conditions.is_git_repo,
-                    init = function(self)
-                        self.status = vim.b.gitsigns_status_dict
-                    end,
-                    intersperse_space({
-                        {
-                            hl = { fg = palette.cyan },
-                            provider = function(self)
-                                return self.status.head:wrap()
-                            end,
-                        },
-                        { git_signs() },
-                    }),
-                }
 
-                local get_diagnostics = function(severity)
-                    return #vim.diagnostic.get(0, { severity = vim.diagnostic.severity[severity] })
+                    return { read_only, truncate, buffer_name }
                 end
-                local count_diagnostics = function(severity)
-                    return normalise_count({ c = get_diagnostics(severity), s = icon, l = " " })
+
+                local Signs = {}
+
+                Signs.__index = Signs
+
+                function Signs:__tostring()
+                    return table.concat({ self.left, self.sign, self.count, self.right })
                 end
-                local diagnostic_sections = function()
-                    local arg = {}
-                    arg.map = {
-                        { type = "ERROR", fg = palette.red_alt_other },
-                        { type = "HINT", fg = palette.blue_alt_other },
-                        { type = "INFO", fg = palette.magenta_alt_other },
-                        { type = "WARN", fg = palette.yellow_alt_other },
+
+                function Signs.new(key, fg, sign)
+                    local t = { key = key, fg = fg, sign = sign, count = 0 }
+                    setmetatable(t, Signs)
+                    return t
+                end
+
+                function Signs:fmt(left, right, sign)
+                    self.left = left
+                    self.right = right
+                    self.sign = sign or self.sign
+                    return self
+                end
+
+                function Signs:define()
+                    return {
+                        hl = { fg = self.fg },
+                        provider = function(line)
+                            self.count = line.status[self.key] or self.count
+                            return self.count ~= 0 and tostring(self)
+                        end,
                     }
-                    arg.f = function(_, v)
-                        return count_diagnostics(v.type)
-                    end
-                    return generate_section(arg)
                 end
-                local diagnostics = {
-                    condition = conditions.has_diagnostics,
-                    diagnostic_sections(),
-                }
+
+                local git = function()
+                    local head = {
+                        hl = { fg = palette.cyan },
+                        provider = function(self)
+                            return self.status.head:wrap()
+                        end,
+                    }
+
+                    local signs = Table.from({
+                        Signs.new("added", palette.green_alt_other, "+"),
+                        Signs.new("changed", palette.yellow_alt_other, "~"),
+                        Signs.new("removed", palette.red_alt_other, "-"),
+                    })
+                        :iter()
+                        :map(function(sign)
+                            return sign:fmt("", " "):define()
+                        end)
+                        :collect()
+
+                    return {
+                        condition = conditions.is_git_repo,
+                        init = function(self)
+                            self.status = {}
+                            setmetatable(self.status, {
+                                __index = vim.b.gitsigns_status_dict,
+                            })
+                        end,
+                        Table.from({ head, signs }):iter():intersperse(space):collect(),
+                    }
+                end
+
+                local diagnostic = function()
+                    local meta = {}
+                    meta.__index = function(_, key)
+                        local status = vim.diagnostic
+                        return #status.get(0, {
+                            severity = status.severity[key],
+                        })
+                    end
+
+                    local signs = Table.from({
+                        Signs.new("ERROR", palette.red_alt_other),
+                        Signs.new("WARN", palette.yellow_alt_other),
+                        Signs.new("INFO", palette.magenta_alt_other),
+                        Signs.new("HINT", palette.blue_alt_other),
+                    })
+                        :iter()
+                        :map(function(sign)
+                            return sign:fmt(" ", "", ICON):define()
+                        end)
+                        :collect()
+
+                    return {
+                        condition = conditions.has_diagnostics,
+                        init = function(self)
+                            self.status = {}
+                            setmetatable(self.status, meta)
+                        end,
+                        signs,
+                    }
+                end
 
                 local position = { provider = "(%l/%L, %v) [%p%%]" }
 
-                local active_statusline = intersperse_space({
-                    mode,
-                    file,
-                    git,
-                    align,
-                    diagnostics,
-                    position,
-                })
-
-                local inactive_statusline = {
-                    condition = function()
-                        return not conditions.is_active()
-                    end,
-                    file,
-                    align,
-                }
-
                 local statusline = {
-                    init = utils.pick_child_on_condition,
                     hl = function()
-                        local style = {
-                            bg = palette.bg_dim,
-                            fg = palette.fg_inactive,
-                        }
-                        if conditions.is_active() then
-                            style.fg = palette.fg_active
-                        end
-                        return style
+                        return { bg = palette.bg_dim, fg = palette.fg_active }
                     end,
-                    inactive_statusline,
-                    active_statusline,
+                    Table.from({ mode(), file(), git(), align, diagnostic(), position })
+                        :iter()
+                        :intersperse(space)
+                        :collect(),
                 }
 
                 require("heirline").setup(statusline)
